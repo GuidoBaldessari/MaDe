39c8624375d99d0264be05a126a4bbef29dfc849
Thomas Andreatta <Thomas.Andreatta@marconi.loc>
2018-03-27 14:44:00 +0200

    First commit

---
+++ js/getData.js
@@ -0,0 +1,247 @@
+
+var server = "http://daas.marconirovereto.it/QROggetti/";
+
+/*function addRow(label, value) {
+    return "<tr><td class='td_sx'>" + label + "</td><td class='td_dx'>" + value + "</td></tr>"
+}*/
+
+/*function createIndex() {
+
+    //var s = "<table border=1 class='idxtable'>";
+
+    for (var i in DB) {
+        cnt = eval(i) + 1;
+        tag = "<a href='#" + DB[i].id_oggetto + "'>" + cnt + ' - ' + DB[i].id_oggetto + "</a>"
+        s += addRow(tag, DB[i].gruppo_made + " - " + DB[i].modello.ITA);
+    }
+    s += "</table>";
+    s += "<br />";
+
+    return s;
+}*/
+
+function MakeImgOggPath(id, img) {
+    return server + id + "/" + img;
+}
+
+function MakeAllegatiPath(id, allegato) {
+    return server + id + "/" + allegato;
+}
+
+function MakeAllegatiURL(id_oggetto, all) {
+    switch (all.tipo) {
+        case 0:
+            return "<audio controls><source src='" + MakeAllegatiPath(id_oggetto, all.url) + "' type='audio/mpeg'></audio>";;
+        case 1:
+            return '<video width="320" height="240" controls><source src="' + MakeAllegatiPath(id_oggetto, all.url) +'" type="video/mp4"></video>'; 
+        case 2:
+            return "<img src='" + MakeImgOggPath(id_oggetto, all.url) + "' width='128px' />"; 
+        case 3:
+            return '<a href="' + all.url + '">' + all.url +'</a>';
+        default:
+            return "???" + url;
+    }
+    return;
+}
+
+/**
+ * 
+ * <div class="third container margin-bottom">
+    <img src="./assets/images/lights.jpg" alt="Norway" style="width:100%" class="hover-opacity">
+    <div class="container white">
+      <p><b>Lorem Ipsum</b></p>
+      <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+    </div>
+    </div>
+ */
+
+function creaOggetto(ogg)
+{
+    
+    var oggetto = document.createElement('div');
+    //var link = document.createElement('a');
+    var img = document.createElement('img');
+    var contenuto = document.createElement('div');
+    var testo = document.createElement('h4');
+    
+   console.log(ogg);
+   //console.log(MakeImgOggPath(ogg.id_oggetto, ogg.url));
+    
+    $(oggetto).attr({
+        'class': 'object third container margin-bottom',
+        'id': ogg.id_oggetto
+    });
+
+    /*$(img).attr({
+        'src': 'http://daas.marconirovereto.it/QROggetti/' +ogg.id_oggetto +'/' +ogg.immagini[0],
+        //'src': MakeImgOggPath(ogg.id_oggetto, all.url),
+        'style': 'width:100%;height:350px;object-fit: cover' 
+    }).appendTo(link);*/
+
+    $(img).attr({
+        'src': 'http://daas.marconirovereto.it/QROggetti/' +ogg.id_oggetto +'/' +ogg.immagini[0],
+        //'src': MakeImgOggPath(ogg.id_oggetto, all.url),
+        'style': 'width:100%;height:350px;object-fit: cover' 
+    }).appendTo(oggetto);
+
+    $(testo).text(ogg.modello.ITA).appendTo(contenuto);
+
+    //$(contenuto).attr('class', 'container white').appendTo(link);
+    $(contenuto).attr('class', 'container white').appendTo(oggetto);
+    
+    /*$(link).attr({
+        'href':'http://daas.marconirovereto.it/QROggetti/' +ogg.id_oggetto +'/' +ogg.immagini[0], //Link pagina dettaglio oggetto
+    }).appendTo(oggetto)*/
+
+    $('#griglia .row-padding:last').append(oggetto);
+
+}
+
+function popolaGriglia()
+{
+    var riga = document.createElement('div');
+    $(riga).attr({
+        'class': 'row-padding',
+    });
+    $('#griglia').append(riga);
+
+
+    //console.log('funzia');
+    for(i = 0; i < DB.length; i++)
+    {
+        //console.log(i);
+        if(i % 3 == 0 && i > 0)
+        {
+            var secondoLivello = document.createElement('div');
+            $(secondoLivello).attr({
+                'class': 'row-padding',
+                'style': 'height: 30%;',
+                'id' : 'row' + i/3
+            }).hide();
+
+            $('#griglia').append(secondoLivello);
+
+            var riga = document.createElement('div');     
+
+            $(riga).attr({
+                'class': 'row-padding',                
+            });
+            $('#griglia').append(riga);
+
+
+        }
+        creaOggetto(DB[i]);
+
+    }
+
+    $(".object").click(function(e) {
+
+        
+
+        var item = $(this); 
+        console.log(item.parent());
+
+        if(!item.hasClass('visualized'))
+        {
+            closeActive();
+
+            item.parent().next().attr({'class':'row-padding active'}).show(); 
+            item.attr({'class':'object third container margin-bottom visualized'});
+            var id = parseInt(item.context.id.split("_")[1]);
+            var ogg = DB[id-1];
+    
+            var lvl2 = item.parent().next().empty();
+            
+            var div = document.createElement('div');
+            var img = document.createElement('img');
+            var contenuto = document.createElement('div');
+            var titolo = document.createElement('h2');
+            var descrizione = document.createElement('p'); 
+            var link = document.createElement('a');
+    
+    
+        
+            //Popola il primo div all'interno della visualizzazione di secondo livello
+            $(img).attr({
+                'src': 'http://daas.marconirovereto.it/QROggetti/' +ogg.id_oggetto +'/' +ogg.immagini[0],
+                'style': 'width:100%;height:350px;object-fit: cover' 
+            }).appendTo(div);
+            $(div).attr({
+                'class': 'third container margin-bottom',
+            }).appendTo(lvl2);
+    
+            
+            //Popola il secondo div all'interno della visualizzazione di secondo livello    
+            $(titolo).text(ogg.modello.ITA).appendTo(contenuto);
+            $(descrizione).html(ogg.descrizione.ITA).appendTo(contenuto);
+            //<a href="#portfolio" onclick="close()" class="bar-item button padding text-teal"><i class="fa fa-th-large fa-fw margin-right"></i>PORTFOLIO</a> 
+            $(link).attr({
+                'class':'bar-item button padding text-teal keep-reading',
+                'href':'file://repo/Studenti$/Informatica/SezAi/4Ai/Thomas.Andreatta/Desktop/MaDe%2026-3-18/object.html?code='+ogg.id_oggetto
+            }).text('Continua a leggere...').appendTo(contenuto);
+            $(contenuto).attr({'class':'twothird container'}).appendTo(lvl2);
+
+        }
+        else
+            {
+                closeActive();
+            }
+      });
+}
+
+/*$('.active').click(function() {
+    closeActive();
+})*/
+
+
+function closeActive()
+{
+    $('.active').attr({'class':'row padding'}).hide();
+    $('.visualized').attr({'class':'object third container margin-bottom'})
+
+}
+
+function popolaSecondoLivello(ogg)
+{
+    var newOgg = ogg;
+    $('#secondoLivello').append(newOgg);
+}
+
+function grigliaConSala(id_sala)
+{
+    for(i = 0; i < DB.length; i++)
+    {        
+        if(DB[i].id_sala == id_sala)
+        {
+            if(i % 3 == 0 || i == 0)
+            {
+                var riga = document.createElement('div');
+                $(riga).attr({
+                    'class': 'row-padding',
+                });
+                $('#griglia').append(riga);
+            }
+            creaOggetto(DB[i]);
+        }
+    }
+}
+
+function grigliaConCollezione(id_collezione)
+{
+    for(i = 0; i < DB.length; i++)
+    {
+        //console.log(i);
+        if(DB[i].id_collezione == id_collezione)
+        {
+            if(i % 3 == 0 || i == 0)
+            {
+                var riga = document.createElement('div');
+                $(riga).attr({
+                    'class': 'row-padding',
+                });
+                $('#griglia').append(riga);
+            }
+            creaOggetto(DB[i]);
+        }
+    }
+}
---
+++ js/getObject.js
@@ -0,0 +1,130 @@
+
+var server = "http://daas.marconirovereto.it/QROggetti/";
+
+function addRow(label, value) {
+    return "<tr><td class='td_sx'>" + label + "</td><td class='td_dx'>" + value + "</td></tr>"
+}
+
+function createIndex() {
+
+    var s = "<table border=1 class='idxtable'>";
+
+    for (var i in DB) {
+        cnt = eval(i) + 1;
+        tag = "<a href='#" + DB[i].id_oggetto + "'>" + cnt + ' - ' + DB[i].id_oggetto + "</a>"
+        s += addRow(tag, DB[i].gruppo_made + " - " + DB[i].modello.ITA);
+    }
+    s += "</table>";
+    s += "<br />";
+
+    return s;
+}
+
+function MakeImgOggPath2(id, img) {
+    return server + id + "/" + id + "-" + img + ".jpg";
+}
+
+function MakeImgOggPath(id, img) {
+    return server + id + "/" + img;
+}
+
+function MakeAllegatiPath(id, allegato) {
+    return server + id + "/" + allegato;
+}
+
+function MakeAllegatiURL(id_oggetto, all) {
+    switch (all.tipo) {
+        case 0:
+            return "<audio controls><source src='" + MakeAllegatiPath(id_oggetto, all.url) + "' type='audio/mpeg'></audio>";;
+        case 1:
+            return '<video width="320" height="240" controls><source src="' + MakeAllegatiPath(id_oggetto, all.url) +'" type="video/mp4"></video>'; 
+        case 2:
+            return "<img src='" + MakeImgOggPath(id_oggetto, all.url) + "' width='128px' />"; 
+        case 3:
+            return '<a href="' + all.url + '">' + all.url +'</a>';
+        default:
+            return "???" + url;
+    }
+    return;
+}
+
+function creaInfoLivello3(ogg) {
+    var s = "";
+
+    s += "<h2>" + ogg.modello.ITA + "</h2><br>";
+    s += "<table style='text-align:left'>";
+    if (ogg.nazione.length > 0) s += "<tr><th width=110px><p><i>Nazione:</i></p></th><td><p>" + ogg.nazione + "</p></td></tr>";
+    if (ogg.data_prod.length > 0) s += "<tr><th><p><i>Data produzione:</i></p></th><td><p>" + ogg.data_prod + "</p></td></tr>";
+    if (ogg.autore.length > 0) s += "<tr><th><p><i>Autore:</i></p></th><td><p>" + ogg.autore + "</p></td></tr>";
+    if (ogg.provenienza.length > 0) s += "<tr><th><p><i>Provenienza:</i></p></th><td><p>" + ogg.provenienza + "</p></td></tr>";
+    if (ogg.calibro.length > 0) s += "<tr><th><p><i>Calibro:</i></p></th><td><p>" + ogg.calibro + "</p></td></tr>";
+    if (ogg.note.length > 0) s += "<tr><th><p><i>Note:</i></p></th><td><p>" + ogg.note + "</p></td></tr>";
+    if (ogg.descrizione.ITA.length > 0) s += "<tr><th style='vertical-align:top'><p><i>Descrizione:</i></p></th><td><p>" + ogg.descrizione.ITA + "</p></td></tr>";
+    if (ogg.misure.ITA.length > 0) s += "<tr><th style='vertical-align:top'><p><i>Misure:</i></p></th><td><p>" + ogg.misure.ITA + "</p></td></tr>";
+    s += "</table>";
+
+
+    /*var testFolder = server + ogg.id;
+    var fs = require('fs');
+    
+    fs.readdirSync(testFolder).forEach(file => {
+      console.log(file);
+    });*/
+
+    s += "<h3 style='text-align:center'>Immagini</h3>";
+    for (var i = 0; i < 4; i++) {
+        s += "<div  class='quarter container margin-bottom'><img src=" + MakeImgOggPath2(ogg.id_oggetto, i+1) + " style='width:100%;height:100%;object-fit: cover'" + " class='hover-opacity'>" /*+ "<div class='container white'><p><b>Immagine</b></p><p>Descrizione.</p></div>" */ + "</div>";
+    }
+
+
+    if (ogg.allegati.length > 0) {
+        s += "<h3 style='text-align:center'>Allegati</h3>";
+        for (var i = 0; i < ogg.allegati.length; i++) {
+            console.log(ogg.allegati[i].tipo);
+            console.log(ogg.allegati[i].url);
+            console.log(ogg.allegati[i].descrizione_breve.ITA);
+        }
+    }
+    
+    return s;
+}
+
+function Trova(stringa){        
+    stringa = stringa.toLowerCase();
+    var ListaElementi = [];
+
+    DB.forEach(function(ogg) 
+    {
+        if(ogg.descrizione.ITA.toLowerCase().search(stringa) != -1 ||
+             ogg.modello.ITA.toLowerCase().search(stringa) != -1 || 
+             ogg.modello.ENG.toLowerCase().search(stringa) != -1 || 
+             ogg.descrizione.ENG.toLowerCase().search(stringa) != -1 ||
+             ogg.nazione.toLowerCase().search(stringa) != -1)
+                {                    
+                    ListaElementi.push(ogg.id_oggetto);
+                }       
+    });
+
+    return ListaElementi;
+}
+
+function getJsonFromUrl() {
+    var query = location.search.substr(1);
+    var result = {};
+    query.split("&").forEach(function(part) {
+      var item = part.split("=");
+      result[item[0]] = decodeURIComponent(item[1]);
+      
+    });
+    return result;
+  }
+
+$(function() {
+    //per la ricerca c'è il metodo Trova, gli si passa la string della barra di ricerca con quel che vogliono cercare e lu la cerca in giro nei determinati campi
+    var result = getJsonFromUrl();    
+    var id = parseInt(result.code.split("_")[1]); 
+
+    $('#info').append(creaInfoLivello3(DB[id-1])); //togliere il numero e lasciare result.id per usare il qr
+});
+
+  
